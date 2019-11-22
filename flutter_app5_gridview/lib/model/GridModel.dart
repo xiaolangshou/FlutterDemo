1554ddb6d0449b608f8f646b7d105ba7f806b717
xiaolangshou <xiaolangshou@foxmail.com>
2019-11-22 16:29:53 +0800

    add grid view demo

---
+++ flutter_app5_gridview/lib/model/GridModel.dart
@@ -0,0 +1,44 @@
+class GridModel {
+  const GridModel({
+    this.title,
+    this.avatar,
+  });
+
+  final String title;
+  final String avatar;
+}
+
+final List<GridModel> gridModels = [
+  GridModel(
+    title: 'thomas lau',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420941145&di=8f27139e7866a585373896cdbbbbd288&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20141114%2FImg406031839.jpg',
+  ),
+  GridModel(
+    title: 'thomas',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420955761&di=2efae8861e309975bfff5f3c10a30dac&imgtype=0&src=http%3A%2F%2Fwww.dzwww.com%2Fnvxing%2Fmr%2F201506%2FW020150627084097244183.jpg',
+  ),
+  GridModel(
+    title: 'liu tao',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420983514&di=5bae97bc9d64cbff0b3744e0bf68b11f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201503%2F22%2F20150322100452_ezcWW.jpeg',
+  ),
+  GridModel(
+    title: 'liu tao',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420983666&di=222728480574877d6ea6d0db21f7a647&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201811%2F16%2F20181116155647_rnjvl.thumb.700_0.jpg',
+  ),
+  GridModel(
+    title: 'liu tao',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420983666&di=992541468afdd4319ea95f3eb91c8da7&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201807%2F09%2F20180709013539_aLhGQ.thumb.700_0.jpeg',
+  ),
+  GridModel(
+    title: 'liu tao',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420983666&di=b0425e96334e1bdf3c3ced92e7d385e2&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201510%2F26%2F20151026093857_PQnmz.jpeg',
+  ),
+  GridModel(
+    title: 'liu tao',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420983666&di=d7180e2d6f5f602cc42e8b3eeae97f1b&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201602%2F23%2F20160223201833_yz8B4.thumb.700_0.jpeg',
+  ),
+  GridModel(
+    title: 'liu tao',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574420983666&di=1b7aac83446b496b67da46c7ba9647c2&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F30%2F20150430125352_aeTLk.jpeg',
+  )
+];
