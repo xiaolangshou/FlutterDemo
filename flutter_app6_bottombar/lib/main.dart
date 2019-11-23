f5d485a3b3d26f438faad8e4fccbc37a9cb497bf
xiaolangshou <xiaolangshou@foxmail.com>
2019-11-23 10:51:19 +0800

    add demo 6

---
+++ flutter_app6_bottombar/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'bottom_navigation_bar_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('Thomas Lau'),
+        ),
+        body: Text('center'),
+        bottomNavigationBar: BottomNavigationBarDemo(),
+      ),
+    );
+  }
+}
