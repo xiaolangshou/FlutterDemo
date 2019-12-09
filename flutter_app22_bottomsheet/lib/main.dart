2c50866953d57209dc4747b38e385b332dfbfab9
xiaolangshou <xiaolangshou@foxmail.com>
2019-12-09 14:16:35 +0800

    add 22 23 demo

---
+++ flutter_app22_bottomsheet/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BottomSheetDemo(),
+    );
+  }
+}
+
+class BottomSheetDemo extends StatefulWidget {
+  @override
+  _BottomSheetDemoState createState() => _BottomSheetDemoState();
+}
+
+class _BottomSheetDemoState extends State<BottomSheetDemo> {
+  final _bottomSheetScaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future _modalBottomSheet() async {
+  final option = await showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 200.0,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('Option A'),
+                  onTap: () {
+                    Navigator.pop(context, 'A');
+                  },
+                ),
+                ListTile(
+                  title: Text('Option B'),
+                  onTap: () {
+                    Navigator.pop(context, 'B');
+                  },
+                ),
+                ListTile(
+                  title: Text('Option C'),
+                  onTap: () {
+                    Navigator.pop(context, 'C');
+                  },
+                )
+              ],
+            ),
+          );
+        }
+        );
+    print(option);
+  }
+
+  _openBottomSheet() {
+    _bottomSheetScaffoldKey.currentState
+        .showBottomSheet((BuildContext context) {
+      return BottomAppBar(
+        child: Container(
+          height: 90.0,
+          width: double.infinity,
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.pause_circle_outline),
+              SizedBox(
+                width: 16.0,
+              ),
+              Text('01:20 / 03:40'),
+              Expanded(
+                child: Text('21 Guns - GreenDay', textAlign: TextAlign.right),
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _bottomSheetScaffoldKey,
+      appBar: AppBar(
+        title: Text('BottomSheetDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                    onPressed: _openBottomSheet,
+                    child: Text('Open BottomSheet')),
+                FlatButton(
+                  onPressed: _modalBottomSheet,
+                  child: Text('Open Modal BottomSheet'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_app23_sanckbar/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SnackBarDemo(),
+    );
+  }
+}
+
+class SnackBarDemo extends StatefulWidget {
+  @override
+  _SnackBarDemoState createState() => _SnackBarDemoState();
+}
+
+class _SnackBarDemoState extends State<SnackBarDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('snack bar demo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SnackBarButton(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SnackBarButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text('Open SnakBar'),
+      onPressed: () {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('on processing...'),
+            action: SnackBarAction(
+              label: 'OK',
+              onPressed: () {
+
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
